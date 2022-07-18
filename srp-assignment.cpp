c99290a709c39f089c2eebd0fbeba57196b04b45
anshumnnit <109504319+anshumnnit@users.noreply.github.com>
2022-07-18 12:59:57 +0530

    Refactored code added

--- srp-assignment.cpp
+++ srp-assignment.cpp
@@ -13,7 +13,37 @@ bool batteryIsOk(float temperature, float soc, float chargeRate) {
   return true;
 }
 
-Observations:
-Code voilates Single responsibility principle.
-Responsibility of temp, SOCCharge and chargeRate can be divided into 3 functions
-Testing of all 3 parameters is difficult.. we need to check multiple of test-case for each functionality.. Resulting in low confidence.
+//Observations:
+//Code voilates Single responsibility principle.
+//Responsibility of temp, SOCCharge and chargeRate can be divided into 3 functions
+//Testing of all 3 parameters is difficult.. we need to check multiple of test-case for each functionality.. Resulting in low confidence.
+
+REFACTORED CODE:
+
+bool BatteryTemp(){
+	if(temperature < 0 || temperature > 45) {
+		cout << "Temperature out of range!\n";
+		return false;
+	}
+	return true;
+}
+
+bool BatterSOCCharge(){
+	if(soc < 20 || soc > 80) {
+    cout << "State of Charge out of range!\n";
+    return false;
+	}
+	return true;
+}
+
+bool BatteryChargeRate(){
+	if(chargeRate > 0.8) {
+    cout << "Charge Rate out of range!\n";
+    return false;
+  }
+  return true;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate){
+  return (BatteryTemp() or BatterSOCCharge() or BatteryChargeRate());
+}
